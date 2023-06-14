f0aeb0821502ca937746496f2ee4e4580e87986e
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-06-14 20:30:46 +0530

    Upgrade to Spring Boot 3.1.0

--- src/test/java/com/sivalabs/bookmarks/common/PostgresDatabaseContainerInitializer.java
+++ src/test/java/com/sivalabs/bookmarks/common/PostgresDatabaseContainerInitializer.java
@@ -8,7 +8,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 public class PostgresDatabaseContainerInitializer
         implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     private static final PostgreSQLContainer<?> sqlContainer =
-            new PostgreSQLContainer<>("postgres:14-alpine")
+            new PostgreSQLContainer<>("postgres:15-alpine")
                     .withDatabaseName("integration-tests-db")
                     .withUsername("sa")
                     .withPassword("sa");
